e68eef698c05c80d8d5480169c6fc78f207dc09b
tomerOko <tomeroko2@gmail.com>
2024-07-29 13:20:57 +0300

    added new calendar

--- src/flows/signin/page/signinPage.tsx
+++ src/flows/signin/page/signinPage.tsx
@@ -1,15 +1,14 @@
 import { SigninForm } from "../components/signinForm";
-
+import { DemoApp } from "../../../common/components/fullCalendar/demo-app";
 
 export const SigninPage = () => {
-
   return (
     <div style={{ textAlign: "center" }}>
       <h1>Welcome Back</h1>
       <div>
         <SigninForm />
+        <DemoApp />
       </div>
     </div>
   );
-
-}
+};
